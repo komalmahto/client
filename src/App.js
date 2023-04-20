040894b700cfa8dc939cb710a7e486bd4cacfc0a
komalmahto <komalmahto22@gmail.com>
2023-04-20 20:17:16 +0530

    privacy and terms and conditions added 7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import Slider from "./Components/Slider.js";
-import Footer from "./Components/Footer";
 import "./App.css";
-import About from "./Components/About";
-import Quizformat from "./Components/Quizformat";
-import Hero from "./Components/Hero";
-import Navbar from "./Components/Navbar";
 import Terms from "./Components/Terms";
 import Privacy from "./Components/Privacy.js";
 import Home from "./Components/Home.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/terms/in-terms" element={<Terms />} />
           <Route path="/policy/in-policy" element={<Privacy />} />
         </Routes>
